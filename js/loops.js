fdd99231a712682fc7991dc0b82510c73dc26bc9
katherine <rusnakkat@gmail.com>
2019-04-29 20:57:48 -0700

    worked with some loops

---
+++ js/loops.js
@@ -0,0 +1,12 @@
+ var fruits =['apple', 'orange', 'strawberry', 'banana', 'pear', 'kiwi']
+//  console.log(i);
+//  for (var i=0; i < fruits.length; i++){
+//      console.log(i);
+//     console.log(fruits[i]);
+//  }
+var index = 0;
+do{
+    console.log(fruits[index]);
+    index ++;
+}
+    while(index<fruits.length);
