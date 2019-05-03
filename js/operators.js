3704c3d0907590ea47c3e344e628d75a491a5525
katherine <rusnakkat@gmail.com>
2019-05-02 19:22:06 -0700

    learned ternary operations as well as boolean objects

--- js/operators.js
+++ js/operators.js
@@ -1,4 +1,48 @@
-// 2 variables with assigned values
-var x=0;
-var y=0;
-if(x>18 && y<21)
+// // 2 variables with assigned values
+// var age = prompt('please enter your age');
+// var accompaniedByAdult = prompt('are you accompanied by an adult');
+// var yes = true;
+// var no = false;
+// if (age >= 18 && age < 21)
+// {
+//     alert('allowed!');
+// }  
+// else if (accompaniedByAdult !='yes')
+//     {
+//         alert('denied');
+//     }
+//         if( age>21)
+//         {
+//             alert('you are free');
+//         }
+//             else if (age<18)
+//             {
+//                 alert('try again next year');
+//             }
+// var age = 19;
+//     accompaniedByAdult = false;
+var age = prompt('enter your age');
+
+if (age >= 18 && age < 21)
+{
+   var accompaniedByAdult = confirm('are you accompanied by an adult');
+    // if(accompaniedByAdult){
+    //     alert('allowed');
+    // }
+    //     else if (accompaniedByAdult == false)
+    //     {
+    //         alert('denied');
+    //     }
+    //ternary operation
+    accompaniedByAdult ? alert('allowed') : alert('denied');
+}
+else if(age>=21){
+    alert('you are free');
+}
+
+    else
+    {
+        alert('try again next year');
+    }
+    // ternary operation for whole check
+    // (age>=18 && age<21) ? (accompaniedByAdult ? alert ('allowed') : alert ('denied')) :(age>=21) ? alert('you are free') : alert('try again next year');
